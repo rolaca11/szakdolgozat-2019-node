aa99f52b8b0540d7117e5b9b640395d6de1ea6a6
Ropi <rolaca11@sch.bme.hu>
2018-12-25 23:26:33 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    $("#menuPanel").on("float.out", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+
+        $("#cover").fadeOut(400);
+    }).on("float.in", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+
+        $("#cover").fadeIn(400);
+    });
+
+    $("*[data-toggle=\"float\"]").on("click", function () {
+        setState($(this));
+    }).each(function () {
+        setState($(this), "init");
+    });
+});
+
+function setState(button, state) {
+    let targetState = button.attr("aria-expanded") === 'true';
+    const target = $(button.attr("data-target"));
+
+    if(state === undefined) {
+        state = "toggle";
+    }
+
+    if(state === "toggle") {
+        targetState = !targetState;
+    }
+
+    if(state === "in") {
+        targetState = true;
+    }
+
+    if(state === "out") {
+        targetState = false;
+    }
+
+    if(targetState) {
+        floatIn(button, target);
+        target.trigger("float.in");
+    } else {
+        floatOut(button, target);
+        target.trigger("float.out");
+    }
+}
+
+function floatIn(button, target) {
+    target.removeClass("out");
+    target.addClass("in");
+
+    button.attr("aria-expanded", "true");
+}
+
+function floatOut(button, target) {
+    target.removeClass("in");
+    target.addClass("out");
+
+    button.attr("aria-expanded", "false");
+}
